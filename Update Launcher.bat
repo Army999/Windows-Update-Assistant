676c95a457a279b6e94f3b3fb06144ae735cefae
Army999 <78039071+Army999@users.noreply.github.com>
2021-04-08 12:54:41 -0400

    Update Update Launcher.bat

--- Update Launcher.bat
+++ Update Launcher.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Copyright (c) Army999
 
-set functions=update
+set functions=update powerconfig
 
-powerconfig
+:powerconfig
 powercfg /SETDCVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
 echo Screen values changed
